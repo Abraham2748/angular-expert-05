a119effdb6a567c302c47867d16370418919aaf9
Abraham2748 <abraham_jack10@hotmail.com>
2025-01-30 21:54:06 -0500

    ng update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,10 @@ import { RouterOutlet } from '@angular/router';
 import { ContadorComponent } from './contador/contador.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, ContadorComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+    selector: 'app-root',
+    imports: [RouterOutlet, ContadorComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-expert-05';
--- src/app/contador/contador.component.ts
+++ src/app/contador/contador.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-contador',
-  standalone: true,
-  imports: [],
-  templateUrl: './contador.component.html',
-  styleUrl: './contador.component.css',
+    selector: 'app-contador',
+    imports: [],
+    templateUrl: './contador.component.html',
+    styleUrl: './contador.component.css'
 })
 export class ContadorComponent {
   contador = 0;
